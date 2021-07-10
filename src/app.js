64934b755701e5a52ec3e904917aa6588bed4bb7
ben16 <matthewbenjamin@northwesternmutual.com>
2021-07-10 16:08:17 -0500

    feat(step5): implement counts in bottom row

--- src/app.js
+++ src/app.js
@@ -10,11 +10,23 @@ const StyledAppContainerDiv = styled.div`
   margin: 0 auto;
 `;
 
+const StyledCountContainerDiv = styled.div`
+  margin-top: 1rem;
+`;
+
+const StyledCountSpan = styled.span`
+  width: 50%;
+  display: inline-block;
+  font-weight: bold;
+`;
+
 function App() {
   const [dataJson, setDataJson] = useState([]);
-  let [nextId, setNextId] = useState(0);
+  const [nextId, setNextId] = useState(0);
   const [hasFetchError, setHasFetchError] = useState(false);
   const [total, setTotal] = useState(new Big("0.00"));
+  const [totalRowCount, setTotalRowCount] = useState(0);
+  const [totalCheckCount, setTotalCheckCount] = useState(0);
 
   useEffect(() => {
     fetchData()
@@ -29,17 +41,24 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const newTotal = dataJson.reduce(
-      (acc, { isChecked, balance: currentBalance }) => {
-        if (isChecked) {
-          return acc.add(currentBalance);
-        }
+    const { runningTotal: newTotal, runningCheckCount: newCheckCount } =
+      dataJson.reduce(
+        (acc, { isChecked, balance: currentBalance }) => {
+          if (isChecked) {
+            acc.runningTotal.add(currentBalance);
+            acc.runningCheckCount = acc.runningCheckCount + 1;
+          }
 
-        return acc;
-      },
-      new Big(0)
-    );
+          return acc;
+        },
+        {
+          runningTotal: new Big(0),
+          runningCheckCount: 0,
+        }
+      );
     setTotal(newTotal);
+    setTotalRowCount(dataJson.length);
+    setTotalCheckCount(newCheckCount);
   }, [dataJson]);
 
   return (
@@ -53,6 +72,10 @@ function App() {
           nextId={nextId}
           setNextId={setNextId}
         />
+        <StyledCountContainerDiv>
+          <StyledCountSpan>Total Row Count: {totalRowCount}</StyledCountSpan>
+          <StyledCountSpan>Check Row Count: {totalCheckCount}</StyledCountSpan>
+        </StyledCountContainerDiv>
       </main>
     </StyledAppContainerDiv>
   );
